2ab106e9aa0db6eda0eab212a8deda2c7bc46c34
eternalBlast <lunar_dr1ve@yahoo.com>
2014-04-05 17:56:50 +0700

    update compability.

--- Muse/Employee.Designer.cs
+++ Muse/Employee.Designer.cs
@@ -100,6 +100,7 @@
             // 
             this.txtPassword.Location = new System.Drawing.Point(99, 100);
             this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(100, 20);
             this.txtPassword.TabIndex = 6;
             // 
--- Muse/Form1.cs
+++ Muse/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Muse
 {
@@ -21,6 +22,11 @@ namespace Muse
         {
             MessageBox.Show("Nama " + txtName.Text + " Alamat " + txtAddress.Text + 
                 " Telepon " + txtPhone.Text + " Gender " + cboGender.Text + " Email " + txtEmail.Text);
+            SqlConnection myConnection = new SqlConnection("user id=username;" + 
+                                       "password=password;server=serverurl;" + 
+                                       "Trusted_Connection=yes;" + 
+                                       "database=database; " + 
+                                       "connection timeout=30");
         }
 
         private void cboGender_KeyPress(object sender, KeyPressEventArgs e)
