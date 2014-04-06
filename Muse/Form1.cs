693f28a08441a7a2e08af2f1a6b8519d38d34656
eternalBlast <lunar_dr1ve@yahoo.com>
2014-04-06 19:17:57 +0700

    Proses insert

--- Muse/Form1.cs
+++ Muse/Form1.cs
@@ -20,13 +20,27 @@ namespace Muse
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Nama " + txtName.Text + " Alamat " + txtAddress.Text + 
+            MessageBox.Show("Nama " + txtName.Text + " Alamat " + txtAddress.Text +
                 " Telepon " + txtPhone.Text + " Gender " + cboGender.Text + " Email " + txtEmail.Text);
-            SqlConnection myConnection = new SqlConnection("user id=username;" + 
-                                       "password=password;server=serverurl;" + 
-                                       "Trusted_Connection=yes;" + 
-                                       "database=database; " + 
+            SqlConnection myConnection = new SqlConnection("user id=andri;" +
+                                       "password=andri;server=WINDOWS7-PC;" +
+                                       "Trusted_Connection=yes;" +
+                                       "database=irestaurant; " +
                                        "connection timeout=30");
+            MessageBox.Show(myConnection.ToString());
+            try
+            {
+                myConnection.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            SqlCommand myCommand = new SqlCommand("INSERT INTO customer (name,created_at,updated_at)" +
+                                     "Values ('Erwin',2014-4-6,2014-4-6)", myConnection);
+            myCommand.Connection = myConnection;
+            myCommand.ExecuteNonQuery();
+
         }
 
         private void cboGender_KeyPress(object sender, KeyPressEventArgs e)
--- Muse/Properties/Settings.Designer.cs
+++ Muse/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Muse.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "10.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,6 +20,16 @@ namespace Muse.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=WINDOWS7-PC;Initial Catalog=irestaurant;Integrated Security=True")]
+        public string irestaurantConnectionString {
+            get {
+                return ((string)(this["irestaurantConnectionString"]));
             }
         }
     }
